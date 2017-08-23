0d3c6cb83f9c8b429e062a822e87634630f202b1
Jack <sjkai1@126.com>
2017-08-23 16:24:25 +0800

    Second commit

--- build.sh
+++ build.sh
@@ -27,7 +27,7 @@ echo "Configuring and building ORB_SLAM2 ..."
 
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=Release -DUSE_MAP_SAVE_LOAD=1
 make
 
 cd ..
